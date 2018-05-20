deadab5fc469b77ee0ed2d72782bc4d60f442b5f
Bret Williams <bret@brets-mbp.tx.rr.com>
2018-05-20 15:30:22 -0500

    ch 3 finish

--- SuperSpaceMan/GameScene.swift
+++ SuperSpaceMan/GameScene.swift
@@ -12,6 +12,10 @@ class GameScene : SKScene {
     
     let backgroundNode = SKSpriteNode(imageNamed: "Background")
     let playerNode = SKSpriteNode(imageNamed: "Player")
+    let orbNode = SKSpriteNode(imageNamed: "PowerUp")
+    
+    let CollisionCategoryPlayer : UInt32 = 0x1 << 1
+    let CollisionCategoryPowerUpOrbs : UInt32 = 0x1 << 2
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -21,16 +25,60 @@ class GameScene : SKScene {
         
         super.init(size: size)
         
+        physicsWorld.contactDelegate = self
+        physicsWorld.gravity = CGVector(dx: 0.0, dy: -2.0)
+        isUserInteractionEnabled = true
+        backgroundColor = SKColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
+        
         backgroundNode.size.width = frame.size.width
         backgroundNode.anchorPoint = CGPoint(x: 0.5, y: 0.0)
         backgroundNode.position = CGPoint(x: size.width / 2.0, y: 0.0)
         addChild(backgroundNode)
         
+        playerNode.physicsBody = SKPhysicsBody(circleOfRadius: playerNode.size.width / 2.0)
+        playerNode.physicsBody?.isDynamic = true
         playerNode.position = CGPoint(x: size.width / 2.0, y: 80.0)
+        playerNode.physicsBody?.linearDamping = 1.0
+        playerNode.physicsBody?.allowsRotation = false
+        playerNode.physicsBody?.categoryBitMask = CollisionCategoryPlayer
+        playerNode.physicsBody?.contactTestBitMask = CollisionCategoryPowerUpOrbs
+        playerNode.physicsBody?.collisionBitMask = 0
         addChild(playerNode)
+        
+        orbNode.physicsBody = SKPhysicsBody(circleOfRadius: playerNode.size.width / 2.0)
+        orbNode.physicsBody?.isDynamic = false
+        orbNode.physicsBody?.categoryBitMask = CollisionCategoryPowerUpOrbs
+        orbNode.physicsBody?.collisionBitMask = 0
+        orbNode.position = CGPoint(x: 170.0, y: size.height - 25)
+        orbNode.name = "POWER_UP_ORB"
+        addChild(orbNode)
         
     }
     
-    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        playerNode.physicsBody?.applyImpulse(CGVector(dx: 0.0, dy: 40.0))
+        
+    }
     
 }
+
+extension GameScene : SKPhysicsContactDelegate {
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        
+        let nodeB = contact.bodyB.node
+        
+        if (nodeB?.name == "POWER_UP_ORB") {
+            
+            nodeB?.removeFromParent()
+            
+        }
+        
+    }
+    
+}
+
+
+
+
